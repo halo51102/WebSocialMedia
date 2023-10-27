c48d960828581a081ee1736e28fb986f459e671c
duynhut908 <dnhut908@gmail.com>
2023-10-27 21:00:43 +0700

    chỉnh sửa file giao diện

--- web_media/api/index.js
+++ web_media/api/index.js
@@ -1,22 +1,28 @@
 import express from 'express'
 const app=express()
 import authRoutes from './routes/auth.js'
-/*import userRoutes from './routes/users.js'
+import userRoutes from './routes/users.js'
 import postRoutes from './routes/posts.js'
 import commentRoutes from './routes/comments.js'
-import likeRoutes from './routes/likes.js'*/
+import likeRoutes from './routes/likes.js'
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
-
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Credentials",true)
+    next()
+})
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin:"http://localhost:3000",
+}
+))
 app.use(cookieParser())
 app.use("/api/auth",authRoutes)
-/*app.use("/api/users",userRoutes)
+app.use("/api/users",userRoutes)
 app.use("/api/posts",postRoutes)
 app.use("/api/comments",commentRoutes)
-app.use("/api/likes",likeRoutes)*/
+app.use("/api/likes",likeRoutes)
 app.listen(8800,() => {
     console.log("API working")
 })
--- web_media/src/context/authContext.js
+++ web_media/src/context/authContext.js
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-
+import axios from "axios"
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
@@ -7,14 +7,13 @@ export const AuthContextProvider = ({ children }) => {
     JSON.parse(localStorage.getItem("user")) || null
   );
 
-  const login = () => {
+  const login = async (inputs) => {
     //TO DO
-    setCurrentUser({
-      id: 1,
-      name: "John Doe",
-      profilePic:
-        "https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    });
+
+    const res= await axios.post("http://localhost:8800/api/auth/login",inputs,{
+    withCredentials:true,
+    })
+    setCurrentUser(res.data)
   };
 
   useEffect(() => {
