ca11a533657305531206ace2b9e4625a801e4a9b
duynhut908 <dnhut908@gmail.com>
2023-12-05 01:10:35 +0700

    done group

--- web_media/api/routes/groups.js
+++ web_media/api/routes/groups.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { getGroup, getAllGroup, addGroup, deleteGroup,getMemberGroup, joinGroup,outGroup } from '../controllers/group.js'
+import { getGroup, getAllGroup, addGroup, deleteGroup,getMemberGroup, joinGroup,outGroup,updateInfoGroup,kichMember,getGroupJoinedIn ,getGroupNoJoin} from '../controllers/group.js'
 const routes=express.Router()
 
+routes.get("/myjoin",getGroupJoinedIn)
+routes.get("/nojoin",getGroupNoJoin)
 routes.get("/",getAllGroup)
 routes.get("/:groupId",getGroup)
 routes.post("/",addGroup)
@@ -9,6 +11,9 @@ routes.delete("/",deleteGroup)
 routes.get("/:groupId/members",getMemberGroup)
 routes.post("/:groupId/members",joinGroup)
 routes.delete("/:groupId/members",outGroup)
+routes.delete("/:groupId/members/:userId",kichMember)
+routes.put("/",updateInfoGroup)
+
 
 
 export default routes
---
+++ web_media/client/src/components/createGroup/CreateGroup.jsx
@@ -0,0 +1,119 @@
+import { useState } from "react";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import "./createGroup.scss"
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
+
+const CreateGroup = ({ setOpenCreate }) => {
+
+    const [img, setImgPic] = useState(null);
+    const [imgCover, setImgCover] = useState(null);
+    const [name, setName] = useState("")
+    const [desc, setDesc] = useState("")
+
+    const upload = async (im) => {
+        try {
+            const formData = new FormData()
+            formData.append("file", im)
+            const res = await makeRequest.post("/upload", formData)
+            return res.data
+        } catch (err) {
+            console.log(err)
+        }
+    }
+
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation(
+        (newGroup) => {
+            return makeRequest.post("/groups", newGroup);
+        },
+        {
+            onSuccess: () => {
+                // Invalidate and refetch
+                queryClient.invalidateQueries(["groups"]);
+            },
+        }
+    );
+    console.log(imgCover)
+    console.log(name)
+    const handleClick = async (e) => {
+        e.preventDefault();
+        let imgUrl = ""
+        let imgCv = ""
+        if (img) imgUrl = await upload(img);
+        if (imgCover) imgCv = await upload(imgCover);
+        console.log(imgCv)
+        mutation.mutate({ profilePic: imgUrl, coverPic: imgCv, desc, name })
+        setOpenCreate(false);
+        setName("")
+        setDesc("")
+        setImgPic(null);
+        setImgCover(null);
+    }
+
+    return (
+        <div className="create">
+            <div className="wrapper">
+                <h1>Add your story</h1>
+                <form>
+                    <div className="files">
+                        <label htmlFor="image">
+                            <span>Image</span>
+                            <div className="imgContainer">
+                                <img
+                                    src={img ? URL.createObjectURL(img) : "No image"}
+                                    alt=""
+                                />
+                                <CloudUploadIcon className="icon" />
+                            </div>
+                        </label>
+                        <input
+                            type="file"
+                            id="image"
+                            style={{ display: "none" }}
+                            onChange={(e) => setImgPic(e.target.files[0])}
+                        />
+                        <label htmlFor="cover">
+                            <span>Image</span>
+                            <div className="imgContainer">
+                                <img
+                                    src={imgCover ? URL.createObjectURL(imgCover) : "No image"}
+                                    alt=""
+                                />
+                                <CloudUploadIcon className="icon" />
+                            </div>
+                        </label>
+                        <input
+                            type="file"
+                            id="cover"
+                            style={{ display: "none" }}
+                            onChange={(e) => setImgCover(e.target.files[0])}
+                        />
+
+                    </div>
+                    <label>Name</label>
+                    <input
+                        type="text"
+                        name="name"
+                        onChange={(e) => setName(e.target.value)}
+                        value={name}
+                    />
+                    <label>Describe</label>
+                    <input
+                        type="text"
+                        name="desc"
+                        onChange={(e) => setDesc(e.target.value)}
+                        value={desc}
+                    />
+                    <button onClick={handleClick}>Add</button>
+                </form>
+                <button className="close" onClick={() => setOpenCreate(false)}>
+                    x
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default CreateGroup;
