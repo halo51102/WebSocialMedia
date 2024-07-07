157899aec9b80ad757c0a0166c0ec00850f8c5fa
halo51102 <20110667@student.hcmute.edu.vn>
2024-07-07 12:56:03 +0700

    Update connect.js

--- web_media/api/connect.js
+++ web_media/api/connect.js
@@ -2,7 +2,6 @@ import mysql from 'mysql'
 
 export const db = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.PORT,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME,
