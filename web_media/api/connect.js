50c56b134ae4a65c4b878b4a7bc36a37beef7dd0
halo51102 <20110667@student.hcmute.edu.vn>
2024-07-10 19:17:56 +0700

    simple face recognition

--- web_media/api/connect.js
+++ web_media/api/connect.js
@@ -1,10 +1,13 @@
 import mysql from 'mysql'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "123456789",
-    database: "socialmedia",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DBNAME,
     multipleStatements: true
 })
 db.connect(function (err) {
